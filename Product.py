a6408dc47dabc9854b3de3e6ffaaf4891187b45c
yashjaiswal1242 <yashjaiswal9@gmail.com>
2026-09-18 13:40:42 -0400

    Product is done

--- Product.py
+++ Product.py
@@ -1,5 +1,6 @@
 
-class Product: 
+
+class product: 
 
     def __init__(self, product_id, name, price): 
         self.product_id = product_id
@@ -16,5 +17,7 @@ class Product:
     def get_price(self): 
         return float(self.price)
 
+    def __repr__(self):
+        return f"Product({self.product_id}) , '{self.name}', ${self.price})"
 
 
--- Shopping Cart.py
+++ Shopping Cart.py
@@ -1,10 +1,11 @@
-import Product
+from Product import product
+
 class ShoppingCart:
     def __init__(self):
-        self.cart = list()
+        self.cart = []
     def add_product(self,product):
         self.cart.append(product)
-    def remove_product(self, product_id: Product.product_id):
+    def remove_product(self, product_id: product.get_id):
         
         if product_id in self.cart:
             self.cart.remove(product_id)
@@ -12,21 +13,30 @@ class ShoppingCart:
         else:
             return False
     def get_items(self):
-      
-        
+
         return self.cart
+
+        
     def calculate_total(self):
         i=0
         total=0
         for i in len(self.cart):
-            total = total + Product.get_price(self.cart(i))
+            total = total + product.get_price(self.cart(i))
             i=i+1
         
         return total
-    def is_empty(self)
+    def is_empty(self):
         if len(self.cart) == 0:
             return True
         else:
             return False
-    
+
+
+s1 = ShoppingCart()
+p1 = product(1, 'Car',100)
+p2 = product(2,'Ball',250)
+s1.add_product(p1)
+print(s1.get_items())
+s1.add_product(p2)
+print(s1.get_items())
     
