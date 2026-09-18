3b47c8e7b6c310144b7b21dcf062b72120a3be49
alex <notalex924@gmail.com>
2026-09-18 19:51:44 -0400

    changed self.cart to self.items

--- cart.py
+++ cart.py
@@ -3,46 +3,46 @@ from product import Product
 class ShoppingCart:
     def __init__(self):
         """Intializes a new empty shoppingcart"""
-        self.cart = []
+        self.items = []
 
 
     def add_product(self,product):
-        """Adds a product object to the shopping cart"""
+        """Adds a product object to the shopping items"""
 
-        self.cart.append(product)
+        self.items.append(product)
 
 
     def remove_product(self, product_id: str):
-        "Removes a product object from the shopping cart if the product_id is available"
+        "Removes a product object from the shopping items if the product_id is available"
 
-        for prod in self.cart: 
+        for prod in self.items: 
             if prod.get_id() == product_id: 
-                self.cart.remove(prod)
+                self.items.remove(prod)
                 return True
         return False
     def get_items(self):
         "Returns all the product objects in the shoppingcart"
 
-        return self.cart
+        return self.items
 
         
     def calculate_total(self):
-        """Calculates and returns the total cost of all the products in the shopping cart"""
+        """Calculates and returns the total cost of all the products in the shopping items"""
 
         total = 0
-        for item in self.cart: 
+        for item in self.items: 
             total += item.get_price()
         return total
     def is_empty(self):
         """Returns True if the car has no items, False otherwise"""
-        if len(self.cart) == 0:
+        if len(self.items) == 0:
             return True
         else:
             return False
 
     def __repr__(self):
-        """Returns string summary of th cart items"""
-        return f'ShoppingCart({self.cart})'
+        """Returns string summary of th items items"""
+        return f'ShoppingCart({self.items})'
 
 
 
