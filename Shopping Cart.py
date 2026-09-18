b9c89039af579d8e8ed4b091656a1937f5bcfedd
alex <notalex924@gmail.com>
2026-09-18 13:18:52 -0400

    finished Shopping Cart

--- Shopping Cart.py
+++ Shopping Cart.py
@@ -2,8 +2,8 @@ import Product
 class ShoppingCart:
     def __init__(self):
         self.cart = list()
-    def add_product(self,product: Product):
-        self.cart.append()
+    def add_product(self,product):
+        self.cart.append(product)
     def remove_product(self, product_id: Product.product_id):
         
         if product_id in self.cart:
@@ -11,5 +11,22 @@ class ShoppingCart:
             return True
         else:
             return False
-    def get_items():
+    def get_items(self):
+      
+        
+        return self.cart
+    def calculate_total(self):
+        i=0
+        total=0
+        for i in len(self.cart):
+            total = total + Product.get_price(self.cart(i))
+            i=i+1
+        
+        return total
+    def is_empty(self)
+        if len(self.cart) == 0:
+            return True
+        else:
+            return False
+    
     
